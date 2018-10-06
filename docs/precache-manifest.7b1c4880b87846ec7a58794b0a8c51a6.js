eaa3123125daebdb5ed3319a228ec2325dcfc81e
mariviru <marivirh82@gmail.com>
2018-10-06 21:16:44 +0200

    create docs folder

---
+++ docs/precache-manifest.7b1c4880b87846ec7a58794b0a8c51a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "364aa410b09e664c11a3",
+    "url": "./static/js/main.364aa410.chunk.js"
+  },
+  {
+    "revision": "e1ada48e35de0e3b8be4",
+    "url": "./static/js/1.e1ada48e.chunk.js"
+  },
+  {
+    "revision": "364aa410b09e664c11a3",
+    "url": "./static/css/main.1676e91d.chunk.css"
+  },
+  {
+    "revision": "eaccfb5f738a72e737bdd5a1b281cc4b",
+    "url": "./index.html"
+  }
+];
